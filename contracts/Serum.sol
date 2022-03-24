24f9a40e5fff4ef3bfbb06ea826a3622dfc24c4e
Minebomber <markmc909@gmail.com>
2022-03-23 20:52:02 -0700

    Add serum token and access control tests

--- contracts/Serum.sol
+++ contracts/Serum.sol
@@ -1,2 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+contract Serum is ERC20, AccessControl, Pausable {
+	bytes32 public constant CONTROLLER_ROLE = keccak256("CONTROLLER_ROLE");
+
+	constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+		_setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+	}
+
+	function mint(address to, uint256 amount) external whenNotPaused onlyRole(CONTROLLER_ROLE) {
+		_mint(to, amount);
+	}
+
+	function burn(address from, uint256 amount) external whenNotPaused onlyRole(CONTROLLER_ROLE) {
+		_burn(from, amount);
+	}
+
+	function addController(address addr) external onlyRole(DEFAULT_ADMIN_ROLE) {
+		grantRole(CONTROLLER_ROLE, addr);
+	}
+
+	function removeController(address addr) external onlyRole(DEFAULT_ADMIN_ROLE) {
+		revokeRole(CONTROLLER_ROLE, addr);
+	}
+
+	function setPaused(bool paused) external onlyRole(DEFAULT_ADMIN_ROLE) {
+		if (paused)	_pause();
+		else        _unpause();
+	}
+}
---
+++ test/Serum.test.js
@@ -0,0 +1,79 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+describe('Serum', function() {
+	before(async function() {
+		this.Serum = await ethers.getContractFactory('Serum');
+		[this.owner, this.other] = await ethers.getSigners();
+	});
+
+	beforeEach(async function() {
+		this.serum = await this.Serum.deploy('Serum', 'SERUM');
+		await this.serum.deployed();
+	});
+
+	it('owner addController success', async function() {
+		await this.serum.connect(this.owner).addController(this.other.address);
+		expect(
+			await this.serum.hasRole(this.serum.CONTROLLER_ROLE(), this.other.address)
+		).to.equal(true);
+	});
+
+	it('non-owner addController revert', async function() {
+		await expect(
+			this.serum.connect(this.other).addController(this.other.address)
+		).to.be.reverted;
+	});
+	
+	it('owner removeController success', async function() {
+		await this.serum.connect(this.owner).addController(this.other.address);
+		await this.serum.connect(this.owner).removeController(this.other.address);
+		expect(
+			await this.serum.hasRole(this.serum.CONTROLLER_ROLE(), this.other.address)
+		).to.equal(false);
+	});
+
+	it('non-owner removeController revert', async function() {
+		await expect(
+			this.serum.connect(this.other).addController(this.other.address)
+		).to.be.reverted;
+	});
+
+	it('non-controller mint revert', async function() {
+		await expect(
+			this.serum.mint(this.other, 1000)
+		).to.be.reverted;
+	});
+	
+	it('controller mint success', async function() {
+		await this.serum.connect(this.owner).addController(this.other.address);
+		await expect(() => 
+			this.serum.connect(this.other).mint(this.other.address, 1000)
+		).to.changeTokenBalance(this.serum, this.other, 1000);
+	});
+	
+	it('non-controller burn revert', async function() {
+		await expect(
+			this.serum.burn(this.other, 1000)
+		).to.be.reverted;
+	});
+	
+	it('controller burn success', async function() {
+		await this.serum.connect(this.owner).addController(this.other.address);
+		await this.serum.connect(this.other).mint(this.other.address, 1000);
+		await expect(() => 
+			this.serum.connect(this.other).burn(this.other.address, 1000)
+		).to.changeTokenBalance(this.serum, this.other, -1000);
+	});
+
+	it('non-owner pause revert', async function() {
+		await expect(
+			this.serum.connect(this.other).setPaused(true)
+		).to.be.reverted;
+	});
+
+	it('owner pause success', async function() {
+		await this.serum.connect(this.owner).setPaused(true);
+		expect(await this.serum.paused()).to.equal(true);
+	})
+});
