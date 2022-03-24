5083bf393bb35ee363530c83e8f6dcf80a748ed2
Minebomber <markmc909@gmail.com>
2022-03-23 20:53:55 -0700

    init contracts

--- contracts/LabGame.sol
+++ contracts/LabGame.sol
@@ -1,2 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+contract LabGame {
+	
+}
