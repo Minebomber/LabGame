1ccbd7fe5e14824c7e75143e84cb661f45192fc8
Minebomber <markmc909@gmail.com>
2022-04-17 22:17:10 -0700

    update solidity version

--- contracts/abstract/Generator.sol
+++ contracts/abstract/Generator.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
+pragma solidity ^0.8.13;
 
 import "./VRFConsumerBaseV2Upgradable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
